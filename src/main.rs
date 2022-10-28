c1b2dc0e5924efc0b11fd6763d2bffea3b2135c2
poseisharp <poseiayayui@gmail.com>
2022-10-28 23:13:22 +0700

    [App] health check endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpServer, Responder, HttpResponse};
+
+
+#[get("/health_check")]
+async fn health_check() -> impl Responder{
+    HttpResponse::Ok()
 }
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(health_check)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
+
